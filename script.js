8b85776e3a3a31b561770899ac20727dbef23694
arawesome02 <ankit.raj@nielsen.com>
2026-01-04 11:06:14 +0530

    Update script.js
    removed

--- script.js
+++ script.js
@@ -1,25 +1,2 @@
 console.log("Launched Successfully.");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const openBtn = document.getElementById('open-btn');
-
-    openBtn.addEventListener('click', () => {
-        const selectedCards = document.querySelectorAll('.card');
-        let count = 0;
-
-        selectedCards.forEach(card => {
-            const checkbox = card.querySelector('.card-checkbox');
-            const link = card.getAttribute('data-link');
-
-            if (checkbox.checked && link) {
-                window.open(link, '_blank');
-                count++;
-            }
-        });
-
-        if (count === 0) {
-            alert("Please select at least one country first.");
-        }
-    });
-});
-
